22f0d7750bc231002da7bcf80cc6c25efc586faa
Bot_Ninja <85632248+NinjaBot17@users.noreply.github.com>
2021-06-10 19:46:12 +0900

    Create Resolve.sh

---
+++ Resolve.sh
@@ -0,0 +1,19 @@
+echo "RESOLVE ERROR TERMUX"
+echo "WITH ME BOT NINJA"
+termux-change-repo
+apt-get install root-repo -y
+apt-get install unstable-repo -y
+apt-get install x11-repo -y
+apt-get install science-repo -y
+apt-get install game-repo -y
+termux-change-repo
+curl -LO https://its-pointless.github.io/setup-pointless-repo.sh
+bash setup-pointless-repo.sh
+termux-change-repo
+apt-get install -y
+apt-get update -y
+apt-get upgrade -y
+echo"Support Me, i'M a Smart Bot yOutuBe"
+echo"Subscribe My ChanneLs"
+echo""
+exit
